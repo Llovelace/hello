74b59875469881ecb1071bdf30e1fba53b4a8a8e
Logan <llovelace2@huskers.unl.edu>
2023-08-24 13:41:25 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,4 +1,18 @@
-* Logan Lovelace
-* 2023/08/22
-* This commands prints my name
+/**
+ * Author: Logan Lovelace
+ *         llovelace2@huskers.unl.edu
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char **argv) {
+
+  printf("Logan Lovelace!\n");
+  printf("Computer Engineering");
+
+  return 0;
+}
